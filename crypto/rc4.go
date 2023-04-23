05cc55b277ac5f52a7350ba29acd451239c8c243
leiwenwei <1207893654@qq.com>
2023-04-23 18:05:12 +0800

    fix: rc4

--- crypto/rc4.go
+++ crypto/rc4.go
@@ -3,7 +3,6 @@ package crypto
 import (
 	"crypto/rc4"
 	"encoding/hex"
-	"strings"
 )
 
 func EncryptRC4(str string, key string) string {
@@ -14,21 +13,20 @@ func EncryptRC4(str string, key string) string {
 		panic(err)
 	}
 	cipher.XORKeyStream(ciphertext, plaintext)
-	return strings.ToUpper(hex.EncodeToString(ciphertext))
+	return hex.EncodeToString(ciphertext)
 }
 
 func DecryptRC4(str string, key string) string {
-	plaintext, err := hex.DecodeString(strings.ToUpper(str))
+	decodeStr, err := hex.DecodeString(str)
 	if err != nil {
 		panic(err)
 	}
-	var decrypted []byte
-	ciphertext := make([]byte, len(plaintext))
+	ciphertext := []byte(decodeStr)
+	plaintext := make([]byte, len(ciphertext))
 	cipher, err := rc4.NewCipher([]byte(key))
 	if err != nil {
 		panic(err)
 	}
-	decrypted = make([]byte, len(ciphertext))
-	cipher.XORKeyStream(decrypted, ciphertext)
-	return string(decrypted)
+	cipher.XORKeyStream(plaintext, ciphertext)
+	return string(plaintext)
 }
