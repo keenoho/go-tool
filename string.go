9af63be3c8b4793ef214f716a1172628230f6ae0
leiwenwei <1207893654@qq.com>
2023-06-19 17:21:03 +0800

    feat: add random string

--- string.go
+++ string.go
@@ -1,10 +1,23 @@
 package tool
 
-import "sort"
+import (
+	"math/rand"
+	"sort"
+)
 
 // StringInList 字符串数组中是否存在目标字符串
 func StringInList(target string, list []string) bool {
 	sort.Strings(list)
 	index := sort.SearchStrings(list, target)
 	return index < len(list) && list[index] == target
+}
+
+// 随机字符串
+func StringRandom(length int) string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~=-")
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
