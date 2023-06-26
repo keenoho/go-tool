55cbebcaa2e2c9a0920baba6cd0cd0b154bc36a9
leiwenwei <1207893654@qq.com>
2023-06-26 17:58:22 +0800

    add isExclude

--- struct.go
+++ struct.go
@@ -1,8 +1,10 @@
 package tool
 
-import "reflect"
+import (
+	"reflect"
+)
 
-func StructConvert(source interface{}, target interface{}) {
+func StructConvert(source interface{}, target interface{}, excludeKeys ...string) {
 	sourceType := reflect.TypeOf(source)
 	sourceValue := reflect.ValueOf(source)
 	targetType := reflect.TypeOf(target).Elem()
@@ -11,6 +13,11 @@ func StructConvert(source interface{}, target interface{}) {
 	for i := 0; i < sourceType.NumField(); i++ {
 		fieldName := sourceType.Field(i).Name
 		fieldValue := sourceValue.FieldByName(fieldName)
+		isExclude := StringInList(fieldName, excludeKeys)
+
+		if isExclude {
+			continue
+		}
 
 		if targetFieldType, ok := targetType.FieldByName(fieldName); ok {
 			targetFieldValue := targetValue.FieldByName(fieldName)
