856da843c90668b70e667c7b339aa07fa0fbf4a6
leiwenwei <1207893654@qq.com>
2023-12-13 14:00:05 +0800

    add func

--- db.go
+++ db.go
@@ -1,7 +1,78 @@
 package tool
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 func DbMakePageSize(page int, size int) (offset int, limit int) {
 	limit = size
 	offset = (page - 1) * size
 	return offset, limit
+}
+
+func DbStructToSqlSelect(stc any) string {
+	stcType := reflect.TypeOf(stc)
+	if stcType.Kind() != reflect.Struct {
+		return "*"
+	}
+	var sql string
+	for i := 0; i < stcType.NumField(); i++ {
+		field := stcType.Field(i)
+		gormTag, isExist := field.Tag.Lookup("gorm")
+		if !isExist {
+			continue
+		}
+		splitArr := strings.Split(gormTag, ";")
+		var column string
+		var table string
+		var originColumn string
+		for _, v := range splitArr {
+			hasColumn := strings.Contains(v, "column:")
+			hasTable := strings.Contains(v, "table:")
+			hasOriginColumn := strings.Contains(v, "originColumn:")
+			if hasColumn {
+				column = strings.ReplaceAll(v, "column:", "")
+			}
+			if hasTable {
+				table = strings.ReplaceAll(v, "table:", "")
+			}
+			if hasOriginColumn {
+				originColumn = strings.ReplaceAll(v, "originColumn:", "")
+			}
+		}
+		if len(column) > 0 && len(table) > 0 && len(originColumn) > 0 {
+			if len(sql) < 1 {
+				sql += fmt.Sprintf("`%s`.`%s` AS `%s`", table, originColumn, column)
+			} else {
+				sql += fmt.Sprintf(" ,`%s`.`%s` AS `%s`", table, originColumn, column)
+			}
+		}
+	}
+	return sql
+}
+
+func DbInsertEntityValue(source any, field string, value any) error {
+	tp := reflect.TypeOf(source)
+	if !(tp.Kind() == reflect.Ptr && tp.Elem().Kind() == reflect.Struct) {
+		return errors.New("the source must be a gorm entity Ptr Struct")
+	}
+	if len(field) < 1 {
+		return errors.New("the format of field is error")
+	}
+
+	elem := tp.Elem()
+	elemField, hasElemField := elem.FieldByName(field)
+	if !hasElemField {
+		return fmt.Errorf("the field %s is not exist in this entity", field)
+	}
+	val := reflect.ValueOf(source)
+	if elemField.Type.Kind() != reflect.ValueOf(value).Kind() {
+		return fmt.Errorf("the value kind is not '%v'", elemField.Type.Kind())
+	}
+	val.Elem().FieldByName(field).Set(reflect.ValueOf(value))
+
+	return nil
 }
--- struct.go
+++ struct.go
@@ -1,9 +1,7 @@
 package tool
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 )
 
 func StructConvert(source interface{}, target interface{}, excludeKeys ...string) {
@@ -29,45 +27,4 @@ func StructConvert(source interface{}, target interface{}, excludeKeys ...string
 			}
 		}
 	}
-}
-
-func StructToSqlSelect(stc any) string {
-	stcType := reflect.TypeOf(stc)
-	if stcType.Kind() != reflect.Struct {
-		return "*"
-	}
-	var sql string
-	for i := 0; i < stcType.NumField(); i++ {
-		field := stcType.Field(i)
-		gormTag, isExist := field.Tag.Lookup("gorm")
-		if !isExist {
-			continue
-		}
-		splitArr := strings.Split(gormTag, ";")
-		var column string
-		var table string
-		var originColumn string
-		for _, v := range splitArr {
-			hasColumn := strings.Contains(v, "column:")
-			hasTable := strings.Contains(v, "table:")
-			hasOriginColumn := strings.Contains(v, "originColumn:")
-			if hasColumn {
-				column = strings.ReplaceAll(v, "column:", "")
-			}
-			if hasTable {
-				table = strings.ReplaceAll(v, "table:", "")
-			}
-			if hasOriginColumn {
-				originColumn = strings.ReplaceAll(v, "originColumn:", "")
-			}
-		}
-		if len(column) > 0 && len(table) > 0 && len(originColumn) > 0 {
-			if len(sql) < 1 {
-				sql += fmt.Sprintf("`%s`.`%s` AS `%s`", table, originColumn, column)
-			} else {
-				sql += fmt.Sprintf(" ,`%s`.`%s` AS `%s`", table, originColumn, column)
-			}
-		}
-	}
-	return sql
 }
