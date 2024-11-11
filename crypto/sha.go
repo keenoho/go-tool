9b9439a3c3248134e3d7ffed5f6dfc4cd5b8770c
leiwenwei <1207893654@qq.com>
2024-11-11 16:09:31 +0800

    add

--- crypto/sha.go
+++ crypto/sha.go
@@ -3,6 +3,7 @@ package crypto
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 )
@@ -14,8 +15,23 @@ func EncryptHMACSHA1(str string, key string) string {
 	return res
 }
 
+func EncryptHMACSHA256(str string, key string) string {
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(str))
+	res := hex.EncodeToString(mac.Sum(nil))
+	return res
+}
+
 func EncryptSHA1(str string) string {
 	h := sha1.New()
+	h.Write([]byte(str))
+	res := h.Sum(nil)
+	return fmt.Sprintf("%x", res)
+
+}
+
+func EncryptSHA256(str string) string {
+	h := sha256.New()
 	h.Write([]byte(str))
 	res := h.Sum(nil)
 	return fmt.Sprintf("%x", res)
